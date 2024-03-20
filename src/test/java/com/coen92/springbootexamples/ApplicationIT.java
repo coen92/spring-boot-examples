d8320df859b92dcf51e8440717fb643d9cb15448
coen92 <piotrkapica92@gmail.com>
2024-03-20 17:01:09 +0100

    SBE-01: initial commit

---
+++ src/test/java/com/coen92/springbootexamples/ApplicationIT.java
@@ -0,0 +1,13 @@
+package com.coen92.springbootexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class ApplicationIT {
+
+    public static void main(String[] args) {
+        SpringApplication.from(Application::main).with(ApplicationIT.class).run(args);
+    }
+
+}
